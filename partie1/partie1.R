2402190bae224d45236ff0761353f514e803d8e2
LorTak <38014349+LorTak@users.noreply.github.com>
2018-05-03 22:27:35 +0200

    Update partie1.R

--- partie1/partie1.R
+++ partie1/partie1.R
@@ -71,7 +71,7 @@ data1[,1]<- NULL
 names(data1)<-c("Alcohol","Malic acid","Ash","Alcalinity of ash","Magnesium","Total phenols","Flavanoids","NonFlavanoid phenols","Proanthocyanins","Color intensity","Hue","OD280/OD315 of diluted wines","Proline")
 
 str(data1)
-shelp(str)
+
 summary(data1)
 ncol(data1)
 nrow(data1)
@@ -81,5 +81,41 @@ boxplot(data1)
 
 PCA<-PCA(data1[,-5])
 plot(PCA)
-p
+
+
+
+res.pca <- PCA(data1)
+
+# On observe que les 2 premiers axes factorielles reprÃ©sente plus de 90 % de l'inertie
+# Le premiere aces est porter par frequency et time.month et le second sur recently.month
+
+pairs(res.pca$ind$coord)
+red <- res.pca$ind$coord
+
+crit <-c("complete","ward.D2","ward.D","average")
+
+d1 <-dist(data1)
+d1 <- dist(red)
+d1 <-dist(red[,1:2])
+pairs(scale(data1),col = cible1+1)
+
+par(mfrow = c(2,2))
+for (c in crit){
+  h <- hclust(d1,method= c)
+  classe = cutree(h,2)
+  t <- table(classe,cible1)
+  plot(red,col=cible1+1)
+}
+
+pairs(red,col=cible1+1)
+
+km <- kmeans(data1,2,nstart = 100)
+table(km$cluster,cible1)
+
+km <- kmeans(red,2,nstart = 100)
+table(km$cluster,cible1)
+
+km <- kmeans(red[,1:2],2,nstart = 100)
+table(km$cluster,cible1)
+
 
